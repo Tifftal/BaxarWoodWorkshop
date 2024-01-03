4ed0aa598f1c7ce983d2b879c2adba2fab6b958d
VarTy <talankina.v@mail.ru>
2024-01-03 23:49:16 +0300

    deploy

--- BWW/src/App.tsx
+++ BWW/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/BaxarWoodWorkshop" element={<Main />} />
         </Routes>
         <Footer />
       </BrowserRouter>
